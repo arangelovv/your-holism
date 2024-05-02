3b8d158c8eb54e5f6af21a14fa01ae1ce657c9c1
rangelovvjobs <rangelovvjobs@gmail.com>
2024-05-02 15:54:28 +0300

    Initial Core Component setup

--- your-holism/src/features/core/Core.tsx
+++ your-holism/src/features/core/Core.tsx
@@ -1,9 +1,32 @@
 import { Outlet } from "react-router-dom";
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
 
 function AppCore() {
   return (
     <div className="core-container">
-      <h1>App Core</h1>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "#2DC27D",
+          paddingLeft: "30px",
+          paddingRight: "20px",
+        }}
+      >
+        <Toolbar sx={{ backgroundColor: "#2DC27D", paddingLeft: "50px" }}>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              color: "#263238",
+              fontWeight: "500",
+            }}
+          >
+            Your Holism
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <Outlet />
     </div>
   );
